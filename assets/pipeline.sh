4e1f51606f1d720fe4fd5b5a7f6e9879a593387f
David <davidcprince01@gmail.com>
2022-05-17 04:49:54 +1200

    COMPX341-22A-A3 Commiting from CI/CD Pipeline

--- assets/pipeline.sh
+++ assets/pipeline.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+commitMsg="COMPX341-22A-A3 Commiting from CI/CD Pipeline"
+
 if ! npm install; then { echo "NPM install failed, aborting."; exit 1; } fi
 if ! npm run build; then { echo "NPM build falied, aborting."; exit 1; } fi
-if ! npm start; then { echo "NPM start failed, aborting"; exit 1; } fi
+
+git add --all
+git commit -m "$commitMsg"
+git push -u origin main
+
+# if ! npm start; then { echo "NPM start failed, aborting"; exit 1; } fi
