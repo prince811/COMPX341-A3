9ebcbb263cf788fda49ebbb83d2aaedcb79b3369
David <davidcprince01@gmail.com>
2022-05-17 05:03:35 +1200

    This is a custom git message

--- assets/pipeline.sh
+++ assets/pipeline.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-commitMsg="COMPX341-22A-A3 Commiting from CI/CD Pipeline"
+[ -z "$1" ] && { echo "Usage: ./pipeline.sh <commit message>"; exit 1; }
 
+commitMsg=$1
+
+echo "(1) Install (npm install)"
 if ! npm install; then { echo "NPM install failed, aborting."; exit 1; } fi
+echo "(2) Build (npm run build)"
 if ! npm run build; then { echo "NPM build falied, aborting."; exit 1; } fi
 
+echo "(3) Adding any changes to git"
 git add --all
+echo "(4) Committing changes to current branch"
 git commit -m "$commitMsg"
+echo "(5) Pushing changes to origin main"
 git push -u origin main
 
+echo "(6) Starting local dev server"
 # if ! npm start; then { echo "NPM start failed, aborting"; exit 1; } fi
