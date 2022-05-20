85d1b8bc5927f803123d4e5a096e046a2bdefbd7
David <davidcprince01@gmail.com>
2022-05-20 15:28:10 +1200

    COMPX341-22A-CommentScript: Added directory check

--- assets/scripts/comment-top-line.sh
+++ assets/scripts/comment-top-line.sh
@@ -1,5 +1,6 @@
 #!bin/bash
-[ -z "$1" ] && { echo "Usage: bash comment-top-line.sh <dir>"; exit 1; }
+! [ -d $1 ] && { echo "Usage: bash comment-top-line.sh <dir>"; echo "Provided argument was not a directory"; exit 1; }
+echo "Working on direcotry $(realpath "$1")"
 echo "Adding Name and ID to the following"
 find $1 -type f -name '*.ts' -printf "%p\n" -exec sed -i '1i // Name: David Prince ID: 1539802' {} +
 echo "Done"
